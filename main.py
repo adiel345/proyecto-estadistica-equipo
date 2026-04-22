495d2b8d3cabcf99fc6c485b24fca96361b08aed
Yordi Jair Euan Euan <yordi.euan_pm25245@cecyte.edu.mx>
2026-04-22 10:47:03 -0600

    Se documenta el codigo de la division de 2 numeros

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 def division(a,b):
     rest = a / b
     return rest
+"""
+Realiza la divison de dos numeros.
+
+Argumentos:
+    a (float): Primer numero a dividir
+    b (float): Segundo numero a dividir
+
+Retorno:
+    (float): Resultado de la divison a y b
+"""
+
 def main():
     print("---Analizador de datos v1.0 ---")
     datos = [10,20,30,40,50,60] #datos de prueba

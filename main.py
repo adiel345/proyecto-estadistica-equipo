77ecf68d788c5176404c87e6a0da341de26f73ed
julian antonio caamal chable <julian.caamal_pm25253@cecyte.edu.mx>
2026-04-23 10:33:27 -0600

    se agrego la documentacion de suma y resta

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 def suma(a,b):
     rest = a + b
     return rest
-    """
-    Realiza la suma de dos numeros.
 
-    Argumentos:
-       a (float): Primer numero a sumar.
-       b (float): Segundo numero a sumar.
+"""
+Realiza la suma de dos numeros.
+
+Argumentos:
+    a (float): Primer numero a sumar.
+    b (float): Segundo numero a sumar.
      
-    retorno:
-       (float): Resultado de la suma a y b.
-    """
+retorno:
+    (float): Resultado de la suma a y b.
+"""
 
 def resta(a,b):
     rest = a - b
     return rest
-    """
-    realiza la resta de los dos numeros.
 
-    Argumentos:
-      a (float): primer numero a restar.
-      b (float): segundo numero a restar.
+"""
+ realiza la resta de los dos numeros.
+
+ Argumentos:
+    a (float): primer numero a restar.
+    b (float): segundo numero a restar.
      
-    retorno:
-      (float): Resultado de la resta de a menos b.
-    """
-
-
+ retorno:
+    (float): Resultado de la resta de a menos b.
+"""
+    
 def main():
     print("---Analizador de datos v1.0 ---")
     datos = [10,20,30,40,50,60] #datos de prueba
@@ -35,4 +36,3 @@ def main():
     #Aqui se llamaran las funciones de los integrantes del equipo
 if __name__=="__main__":
     main()
-    #
